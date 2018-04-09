92480762f771869e52b0e51ee6eb98f164d16764
Eric Jiang <ejiang@amazon.com>
2018-04-08 23:44:07 -0700

    Retry WebSocket on disconnect.

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const WEBSOCKET_ENDPOINT = 'wss://frontiersmen.herokuapp.com';
-// export const WEBSOCKET_ENDPOINT = 'ws://localhost:4567';
+// export const WEBSOCKET_ENDPOINT = 'wss://frontiersmen.herokuapp.com';
+export const WEBSOCKET_ENDPOINT = 'ws://localhost:4567';
+export const WEBSOCKET_RETRY_DELAY = 5 * 1000;
--- src/util/webSocketConnection.js
+++ src/util/webSocketConnection.js
@@ -1,31 +1,36 @@
 import { WEBSOCKET_ENDPOINT } from '../config.js';
+import { WEBSOCKET_RETRY_DELAY } from '../config.js';
 
 export default class WebSocketConnection {
   constructor(playerId, authToken, targetName, path, params) {
-    var url = `${WEBSOCKET_ENDPOINT}/ws/${path}?playerId=${playerId}&authToken=${authToken}`;
-    if (params) {
-      for (var param in params) {
-        url += `&${param}=${params[param]}`;
+    var createWebSocket = () => {
+      var url = `${WEBSOCKET_ENDPOINT}/ws/${path}?playerId=${playerId}&authToken=${authToken}`;
+      if (params) {
+        for (var param in params) {
+          url += `&${param}=${params[param]}`;
+        }
+      }
+      this.ws = new WebSocket(url);
+
+      this.ws.onopen = () => {
+        console.log(`Opened WebSocket connection to ${targetName}`);
+      };
+
+      this.ws.onclose = (closeEvent) => {
+        console.log(`WebSocket connection to ${targetName} was closed. (Reason: ${closeEvent.reason})`);
+        setTimeout(createWebSocket, WEBSOCKET_RETRY_DELAY);
+      }
+
+      this.ws.onmessage = (message) => {
+        var event = JSON.parse(message.data);
+        console.log(event);
+        if (this.eventHandlers[event.eventType]) {
+          this.eventHandlers[event.eventType].forEach(handler => handler(event));
+        }
       }
     }
-    this.ws = new WebSocket(url);
 
-    this.ws.onopen = () => {
-      console.log(`Opened WebSocket connection to ${targetName}`);
-    };
-
-    this.ws.onclose = (closeEvent) => {
-      console.log(`WebSocket connection to ${targetName} was closed. (Reason: ${closeEvent.reason})`);
-    }
-
-    this.ws.onmessage = (message) => {
-      var event = JSON.parse(message.data);
-      console.log(event);
-      if (this.eventHandlers[event.eventType]) {
-        this.eventHandlers[event.eventType].forEach(handler => handler(event));
-      }
-    }
-
+    createWebSocket();
     this.eventHandlers = {};
   }
 
